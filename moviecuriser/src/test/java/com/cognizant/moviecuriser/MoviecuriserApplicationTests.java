914e4e2d3db8200e29b8d306d14dbaf5f733156e
princi-pk <75043174+princi-pk@users.noreply.github.com>
2021-01-31 23:19:21 +0530

    Add files via upload

---
+++ moviecuriser/src/test/java/com/cognizant/moviecuriser/MoviecuriserApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cognizant.moviecuriser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * 
+ * @author Ayush Srivastava
+ *
+ */
+@SpringBootTest
+class MoviecuriserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
